1aafc4e04940817b85313e372c2a51b59b04c15e
Ganesh Kanchi <ganeshkanchimad@gmail.com>
2022-01-11 20:53:55 +0530

    trimmed code using ES6

--- index.js
+++ index.js
@@ -2,35 +2,18 @@ const btnCheck = document.querySelector("#btn-check");
 const result = document.querySelector("#result");
 result.style.display = "none";
 
-btnCheck.addEventListener("click", function onClick() {
+btnCheck.addEventListener("click", () => {
     const luckyNumber = document.querySelector("#lucky-number");
     const birthday = document.querySelector("#birth-date");
-
     var birthdaySum = 0;
-    const birthdayValue = (birthday.value.replaceAll("-", ""));
+    const birthdayValue = (birthday.value.replaceAll("-", "")).split("");
+    birthdayValue.forEach(element => {
+        birthdaySum+=element
+    });
 
-    if (luckyNumber.value === "" || birthday.value === "") {
-        result.style.display = "block"
-        result.innerText = "You have not provided the required Information."
-    } else {
-        if (Number(luckyNumber.value) === 0 || Number(luckyNumber.value) > 10) {
-            result.style.display = "block"
-            result.innerText = "Lucky Number must be greater than 0 and less then 10."
-        } else {
-            for (i = 0; i < 8; i++) {
+    result.innerText = (luckyNumber.value === "" || birthday.value === "") ? "You have not provided the required Information." : (Number(luckyNumber.value) === 0 || Number(luckyNumber.value) > 10) ? "Lucky Number must be greater than 0 and less then 10." : (Math.trunc((birthdaySum % Number(luckyNumber.value)) === 0)) ? "Congratulations, Your Birthday is Lucky!" : "Alas, Your Birthday is not Lucky.";
+    
+    result.style.display = (luckyNumber.value === "" || birthday.value === "") ? "block" : (Number(luckyNumber.value) === 0 || Number(luckyNumber.value) > 10) ? "block" : (Math.trunc((birthdaySum % Number(luckyNumber.value)) === 0)) ? "block" : "block";
 
-                birthdaySum = birthdaySum + (Number(birthdayValue.slice(i, (i + 1))));
-            };
-            if (Math.trunc((birthdaySum % Number(luckyNumber.value)) === 0)) {
-                result.style.display = "block"
-                result.innerText = "Congratulations, Your Birthday is Lucky!"
-                // console.log("Congratulations, Your Birthday is Lucky!");
-            } else {
-                result.style.display = "block"
-                result.innerText = "Alas, Your Birthday is not Lucky."
-                // console.log("Alas, Your Birthday is not Lucky.");
-            }
-        }
-    }
 
 });
